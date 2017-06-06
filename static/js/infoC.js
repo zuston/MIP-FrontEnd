c9e59e7f3a40894515305827a6f2446126a3b264
zuston.shacha <zuston.shacha@gmail.com>
2017-06-07 00:26:43 +0800

    增加info的检索

---
+++ static/js/infoC.js
@@ -0,0 +1,46 @@
+function getUrlParam(name) {
+	        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+	        var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+	        if (r != null) return unescape(r[2]); return null; //返回参数值
+	    }
+	 function clickRadio(tmpType){
+		 if(tmpType=="n_type"){
+			 $("#neg").css('visibility') == 'hidden';
+		 }
+		 if(tmpType=="p_type"){
+			 $("#neg").css('visibility') == 'visible';
+		 }
+	 }
+
+	function clickBtn(){
+		var tmpdt = new Object();
+		alert($("#p_type").is(':checked'));
+		tmpdt["extract_id"] = getUrlParam('extract_id');
+		tmpdt["T"] = $("#T").val();
+		tmpdt["carr"] = $("#carr").val();
+		tmpdt["deformation_potential"] = $("#deformation_potential").val();
+		tmpdt["elastic_constant"] = $("#elastic_constant").val();
+		$.ajax({
+			 url : "http://nova.shu.edu.cn/SpringMVC/ajax_trans_calculate.do",
+			 /* url : "http://localhost:8080/SpringMVC/ajax_trans_calculate.do",   */
+			type : 'post',
+			dataType : "json",
+			contentType : "application/json",
+			data : JSON.stringify(tmpdt),
+			success : function(data, status) {
+				if (status == "success") {
+					/* alert(data.folderName);
+					alert(data.pf);
+					alert(data.tp); */
+					$("#tp").html(data.tp);
+					$("#pf").html(data.pf);
+					$("#downloadBtn").attr("href","http://nova.shu.edu.cn/SpringMVC/downloadbxsf.do?folderName="+data.folderName);
+					$("#resultDiv").show();
+				}
+			},
+			error : function(xhr, textStatus,
+					errorThrown) {
+				alert(errorThrown);
+			}
+		});
+	}
